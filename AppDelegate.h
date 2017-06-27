6d209fd0cf660ff60a76f1f396bbd0d21b1b7b14
wzpziyi1 <1204473090@qq.com>
2017-06-27 15:51:30 +0800

    初始文件

---
+++ AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  ZYDownloader
+//
+//  Created by 王志盼 on 2017/6/27.
+//  Copyright © 2017年 王志盼. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZYDownloader
+//
+//  Created by 王志盼 on 2017/6/27.
+//  Copyright © 2017年 王志盼. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
